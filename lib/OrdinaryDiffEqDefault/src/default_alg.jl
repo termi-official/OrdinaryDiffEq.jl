b92337c2e0f4ff259fb8d947c4f681176fa75618
termi-official <termi-official@users.noreply.github.com>
2026-02-11 19:30:23 +0100

    Make DefaultAlg use PIController

--- lib/OrdinaryDiffEqDefault/src/default_alg.jl
+++ lib/OrdinaryDiffEqDefault/src/default_alg.jl
@@ -47,7 +47,17 @@ function isdefaultalg(
     return true
 end
 
+<<<<<<< HEAD
 SciMLBase.supports_solve_rng(::SciMLBase.AbstractODEProblem, ::Nothing) = true
+=======
+function OrdinaryDiffEqCore.default_controller(
+        QT, alg::CompositeAlgorithm{
+            <:Any, <:Tuple{Tsit5, Vern7, Rosenbrock23, Rodas5P, FBDF, FBDF},
+        }
+    )
+    return PIController(float(DEFAULTBETA1S[1]), float(DEFAULTBETA2S[1]))
+end
+>>>>>>> ce2d5a424 (Make DefaultAlg use PIController)
 
 function SciMLBase.__init(prob::ODEProblem, ::Nothing, args...; kwargs...)
     return SciMLBase.__init(
